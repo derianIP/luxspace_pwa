5205851c0170163c833570ca55a3e4b2678d2e26
Derian Indra Pratama <derian.pratama@edi-indoensia.co.id>
2021-04-28 12:54:38 +0700

    Migrasi dari templete ke component react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Arrived,
+  AsideMenu,
+  Browse,
+  Clients,
+  Footer,
+  Header,
+  Hero,
+} from "./components/molecules";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <Browse />
+      <Arrived />
+      <Clients />
+      <AsideMenu />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/molecules/index.js
@@ -0,0 +1,9 @@
+import Header from "./Header";
+import Hero from "./Hero";
+import Browse from "./Browse";
+import Arrived from "./Arrived";
+import Clients from "./Clients";
+import AsideMenu from "./AsideMenu";
+import Footer from "./Footer";
+
+export { Header, Hero, Browse, Arrived, Clients, AsideMenu, Footer };
